de8b4416554065809e5987335b75538a50d43edf
Isis Lázaro <isislazaro@gmail.com>
2020-02-04 09:33:31 -0600

    changing computed properties por title and displayed image

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const app = new Vue({
   el: "#app",
   data: {
+    brand: "Vue Mastery",
     product: "Socks",
     description: "A pair of warm fuzzy socks socks",
-    image: "./assets/vmSocks-green-onWhite.jpg",
+    // image: "./assets/vmSocks-green-onWhite.jpg",
+    selectedVariant: 0,
     image_alt: "green socks with Vue logo",
-    inStock: true,
+    // inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [
       {
         variantId: 2234,
         variantColor: "green",
-        variantImage: "./assets/vmSocks-green-onWhite.jpg"
+        variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        variantQuantity: 10
       },
       {
         variantId: 2235,
         variantColor: "blue",
-        variantImage: "./assets/vmSocks-blue-onWhite.jpg"
+        variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        variantQuantity: 0
       }
     ],
     cart: 0
@@ -25,8 +29,20 @@ const app = new Vue({
     addToCart() {
       this.cart += 1;
     },
-    updateProduct(variantImage) {
-      this.image = variantImage;
+    updateProduct(index) {
+      // this.image = variantImage;
+      (this.selectedVariant = index), console.log(index);
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
     }
   }
 });
